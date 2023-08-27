fcad5f1703b62ace1d12a2ed6270e9e7bf7570d2
bosungpark <qkrqhtjd0806@naver.com>
2023-08-28 00:53:15 +0900

    func return

--- sources/syntax_examples/function.go
+++ sources/syntax_examples/function.go
@@ -14,10 +14,18 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println(add(42, 13))  // 55
 	fmt.Println(minus(42, 12))  // 30
 
-	a, b := swap("hello", "world") 
+	a, b := swap("hello", "world") // fmt.Println(swap("hello", "world"))
 	fmt.Println(a, b)
+
+	fmt.Println(split(17)) // 7 10
 }
