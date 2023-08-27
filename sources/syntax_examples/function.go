ba7174197303dc70e2ed4ccbc540835115337e5f
bosungpark <qkrqhtjd0806@naver.com>
2023-08-28 00:49:49 +0900

    func

--- sources/syntax_examples/function.go
+++ sources/syntax_examples/function.go
@@ -6,6 +6,18 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func minus(x, y int) int {
+	return x - y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(42, 13))  // 55
+	fmt.Println(minus(42, 12))  // 30
+
+	a, b := swap("hello", "world") 
+	fmt.Println(a, b)
 }
