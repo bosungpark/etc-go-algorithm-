13068dfa34747f26ff2e1498844d50bb9a83235b
bosungpark <qkrqhtjd0806@naver.com>
2023-09-03 03:58:06 +0900

    pow

---
+++ sources/simple-blockchain/proof-of-work.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+	"bytes"
+	"encoding/binary"
+	"strconv"
+	"time"
+	"math"
+)
+
+
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to bosung")
+	bc.AddBlock("Send 2 more BTC to bosung")
+
+	for i, block := range bc.blocks {
+		fmt.Printf("%d: Prev. hash: %x\n", i, block.PrevBlockHash)
+		fmt.Printf("%d: Data: %s\n", i, block.Data)
+		fmt.Printf("%d: Hash: %x\n", i, block.Hash)
+		fmt.Println()
+
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+}
+
+
+// block that store valuable information.
+type Block struct {
+	Timestamp     int64  // when the block is created
+	Data          []byte  // actual valuable information containing in the block
+	PrevBlockHash []byte  // the hash of the previous block
+	Hash          []byte  // the hash of the block
+	Nonce		  int  // count
+}
+
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
+
+// 	b.Hash = hash[:]
+// }
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+	return block
+}
+
+// blockchain
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+// Proof-of-Work algorithms must meet a requirement: doing the work is hard, but verifying the proof is easy
+
+const targetBits = 24  // the block header storing the difficulty at which the block was mined.
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int  // another name for the requirement 
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	binary.Write(buff, binary.BigEndian, num)
+
+	return buff.Bytes()
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
