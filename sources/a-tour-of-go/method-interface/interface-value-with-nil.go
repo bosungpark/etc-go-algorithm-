3e82a48c26a2f70aa849243bea0de306894576a4
bosungpark <qkrqhtjd0806@naver.com>
2023-09-04 00:56:03 +0900

    empty interface

--- sources/a-tour-of-go/method-interface/interface-value-with-nil.go
+++ sources/a-tour-of-go/method-interface/interface-value-with-nil.go
@@ -22,8 +22,15 @@ func describe(i I) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
 
+func describe_empty_interface(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
 func main() {
 	var i I
+
+	describe(i)  // (<nil>, <nil>)
+	// i.M()  // panic: runtime error: invalid memory address or nil pointer dereference
 
 	var t *T
 	i = t
@@ -33,4 +40,14 @@ func main() {
 	i = &T{"hello"}  
 	describe(i)  // (&{hello}, *main.T)
 	i.M()  // hello
+
+	// 빈 인터페이스는 모든 유형의 값을 가질 수 있습니다. (모든 유형은 최소 0개의 메소드를 구현합니다.)
+	var e interface{}
+	describe_empty_interface(e)  // (<nil>, <nil>)
+
+	e = 42
+	describe_empty_interface(e)  // (42, int)
+
+	e = "hello"
+	describe_empty_interface(e)  // (hello, string)
 }
